613f6f0e053d01ddb657373ecf41da88e2dbab4d
Adopolis23 <weinhofer@mail.usf.edu>
2022-12-29 16:05:23 -0500

    got image loading and parsing working

--- NeuralNetImageClassifier/main.py
+++ NeuralNetImageClassifier/main.py
@@ -7,9 +7,16 @@ pp = pprint.PrettyPrinter(indent=4)
 import joblib
 from skimage.io import imread
 from skimage.transform import resize
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import SGDClassifier
 
 
 
+data = dict()
+data['description'] = "10 x 10 image either black or white"
+data['label'] = []
+data['filename'] = []
+data['data'] = []  
 
 
 directory = 'IMAGES'
@@ -18,6 +25,40 @@ directory = 'IMAGES'
 # that directory
 for filename in os.listdir(directory):
     f = os.path.join(directory, filename)
-    # checking if it is a file
-    if os.path.isfile(f):
-        print(filename)
+    
+
+    im = imread(f)
+
+    data['label'].append(filename)
+    data['filename'].append(filename)
+    data['data'].append(im)
+
+labels = np.unique(data['label'])
+fig, axes = plt.subplots(1, len(labels))
+fig.set_size_inches(15,4)
+fig.tight_layout()
+
+for ax, label in zip(axes, labels):
+    idx = data['label'].index(label)
+     
+    ax.imshow(data['data'][idx])
+    ax.axis('off')
+    ax.set_title(label)
+
+
+X = np.array(data['data'])
+y = np.array(data['label'])
+
+new_x = []
+
+for item in X:
+    FLAT = [item for sublist in item for item in sublist]
+    new_x.insert(-1, FLAT)
+
+    
+sgd_clf = SGDClassifier(random_state=42, max_iter=1000, tol=1e-3)
+sgd_clf.fit(new_x, y)
+
+
+
+
